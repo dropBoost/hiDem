562276a694a2d7f664ec85a956d8041b6543b5f7
DropBoost <dev@dropboost.it>
2025-11-02 22:06:34 +0100

    test home

--- src/app/gestionale/page.jsx
+++ src/app/gestionale/page.jsx
@@ -7,8 +7,8 @@ import MENUhomepage from "@/app/componenti/menuHome"
 export default function Dashboard() {
 
   return (
-    <div className="flex flex-col gap-3 w-full h-full border">
-      <div className="p-5 rounded-lg overflow-auto">
+    <div className="flex flex-col gap-3 w-full max-h-screen border">
+      <div className="p-5 rounded-lg max-h-screen overflow-auto border border-red-700">
         <MENUhomepage/>
       </div>
     </div>
